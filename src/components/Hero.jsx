31aa3675c03000cfa0bd7af7496a80574fad85cb
muheirwegillian <muheirwegillian@gmail.com>
2026-08-13 23:07:15 -0700

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className='text-white'>
+            <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+                <p className='text-[#00df9a] font-bold p-2'>GROWING WITH DATA ANALYTICS</p>
+                <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow with data.</h1>
+                <div>
+                <p className='md:text-2xl text-xl font-bold text-gray-500'>Fast, flexible financing for your business.</p>
+                 </div>   
+            </div>
+        </div>
+    )
+}
+
+export default Hero
