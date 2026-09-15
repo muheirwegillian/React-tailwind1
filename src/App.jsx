ca40bc0f0218a7154a0fe722d9e877ff1702097d
muheirwegillian <muheirwegillian@gmail.com>
2026-09-15 20:08:38 +0300

    update.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import Analytics from './components/Analytics';
+import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Newsletter from './components/Newsletter';
+
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero/>
+      <Analytics/>
+      <Newsletter/>
+      <Cards/>
     </div>
   );
 }
